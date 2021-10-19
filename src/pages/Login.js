2dda26e31ed8be35e615b70433f98fafc119787c
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-10-19 16:08:09 +0100

    refactor: add validation to login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,39 +1,85 @@
 import React from "react";
 import logo from "images/logo.svg";
+import {
+  ErrorMessage,
+  FormGroup,
+  Input,
+  Label,
+  CheckBox,
+  Button,
+} from "components";
+
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+  remember: yup.bool()
+});
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleLogin = (data) => console.log(data);
+
   return (
     <div className="grid h-screen bg-gradient-to-br from-indigo-800 via-blue-500 to-blue-900 place-items-center">
-      <div class="container mx-auto flex flex-wrap items-center">
-        <div class="lg:w-2/6 md:w-1/2 bg-white rounded-xl p-8 flex flex-col m-4 md:mx-auto w-full mt-10 md:mt-0">
+      <div className="container flex flex-wrap items-center mx-auto">
+        <div className="flex flex-col w-full p-8 m-4 mt-10 bg-white lg:w-2/6 md:w-1/2 rounded-xl md:mx-auto md:mt-0">
           <div className="mb-4">
             <img src={logo} alt="logo" className="mx-auto h-36" />
             <h1 className="text-3xl text-center">Developer Console</h1>
           </div>
-          <div class="relative mb-4">
-            <label for="email" class="leading-7  text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-800 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div class="relative mb-4">
-            <label for="full-name" class="leading-7  text-gray-600">
-              Password
-            </label>
-            <input
-              type="password"
-              name="full-name"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-800 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button class="text-white bg-gradient-to-br from-indigo-800 via-blue-500 to-blue-900 hover:from-blue-500 hover:via-indigo-800 hover:to-blue-500 border-0 py-2 px-8 focus:outline-none rounded text-lg">
-            login
-          </button>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <FormGroup>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                {...register("email")}
+                error={errors.email}
+              />
+              {errors.email && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                {...register("password")}
+                error={errors.password}
+              />
+              {errors.password && <ErrorMessage>{errors.password?.message}</ErrorMessage>}
+            </FormGroup>
+
+            <FormGroup>
+
+
+              <Controller
+                control={control}
+                name="rememberMe"
+                render={({ field: { value, onChange } }) => (
+                  <Label className="inline-flex items-center">
+                    <CheckBox
+                      value={value}
+                      onChange={onChange}
+                    />
+                    <span className="ml-2">remember me</span>
+                  </Label>
+                )}
+              />
+            </FormGroup>
+            <Button>login</Button>
+          </form>
         </div>
       </div>
     </div>
